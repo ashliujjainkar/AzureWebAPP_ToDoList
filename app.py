eaa45028bb308552980ee4896049974f0dfa6720
Ashli Ujjainkar <ashliujjainkar@gmail.com>
2024-11-24 17:17:22 +0530

    update home page

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def task_serializer(task):
 def get_version():
     return jsonify({"version": api_version})
 
+@app.route('/', methods=['GET'])
+def get_home():
+    return "Hello! Welcome to Todo List App"
+
+
 @app.route('/list',methods = ['GET'])
 def list_tasks():
     return jsonify([task_serializer(task) for task in tasks.values()])

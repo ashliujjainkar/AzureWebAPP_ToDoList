73dd3da0db1ea9ba4b849c7e0e440d08801a98d1
Ashli Ujjainkar <ashliujjainkar@gmail.com>
2024-11-25 18:39:03 +0530

    add debug mode

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def dump():
     return jsonify({"message": f"{len(tasks)} tasks stored in Cosmos DB"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888)
+    app.run(host='0.0.0.0', port=8888, debug = True)
